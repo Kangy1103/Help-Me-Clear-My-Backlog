40e331c31688218f4f5ad7fb36067ae4417c8c74
Kangy <joe.tolchard@gmail.com>
2026-07-20 20:28:46 +0100

    Test linking steam library search with HLTB

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 
 from dotenv import load_dotenv
@@ -36,25 +37,30 @@ def main():
         }
         for game in games_list
     ]
-    for game in skimmed_library:
-        print("==============================")
-        console.print(f"Game: {game.get("name")}")
-        console.print(f"Playtime: {game.get("playtime") / 60:.2f} hours")
-        print("==============================")
+    # for game in skimmed_library:
+    #     print("==============================")
+    #     console.print(f"Game: {game.get("name")}")
+    #     console.print(f"Playtime: {game.get("playtime") / 60:.2f} hours")
+    #     print("==============================")
 
     # Once Steam API is implemented or planned, refactor the below so this file doesn't get too big!
     print("Searching for game...")
-    with Progress(transient=True) as progress:
+    with Progress(console=console, transient=True) as progress:
         progress.add_task("", total=None)
-        results = HowLongToBeat().search("")
-    if not results:
-        console.print(f"[bold red]No game found[/bold red]")
-    else:
-        best = max(results, key=lambda e: e.similarity)
-        print("[bold green]Game found![/bold green]")
-        console.print(
-            f"[bold blue]{best.game_name}[/bold blue] — [bold green]Main: [/bold green][white]{best.main_story}h,[/white] [bold green]Completionist: [/bold green][white]{best.completionist}h[/white]"
-        )
+        for game in skimmed_library:
+            game_name = game.get("name")
+            playtime = game.get("playtime")
+            if not game_name:
+                continue
+            results = HowLongToBeat().search(game_name)
+            if not results:
+                console.print(f"[bold red]No game found:[/bold red] {game_name}")
+            else:
+                best = max(results, key=lambda e: e.similarity)
+                progress.console.print("[bold green]Game found![/bold green]")
+                progress.console.print(
+                    f"[bold blue]{best.game_name}[/bold blue] — Playtime: {playtime /60:.2f} hours -  [bold green]Main: [/bold green][white]{best.main_story}h,[/white] [bold green]Completionist: [/bold green][white]{best.completionist}h[/white]"
+                )
 
 
 if __name__ == "__main__":
