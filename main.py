16c72143f45747375ad9fb07aba2b160df0d3ed0
Kangy <joe.tolchard@gmail.com>
2026-07-22 00:11:16 +0100

    Test asyncio for querying HLTB

--- main.py
+++ main.py
@@ -14,7 +14,25 @@ KEY = os.environ.get("STEAM_API_KEY")
 steam = Steam(KEY)
 
 
-def main():
+async def fetch_game_data(game, semaphore, progress):
+    async with semaphore:
+        game_name = game.get("name")
+        playtime = game.get("playtime")
+        if not game_name:
+            return
+        results = await HowLongToBeat().async_search(game_name)
+        if not results:
+            console.print(f"[bold red]No game found:[/bold red] {game_name}")
+        else:
+            best = max(results, key=lambda e: e.similarity)
+            progress.console.print("[bold green]Game found![/bold green]")
+            progress.console.print(
+                f"[bold blue]{best.game_name}[/bold blue] — Playtime: {playtime /60:.2f} hours -  [bold green]Main: [/bold green][white]{best.main_story}h,[/white] [bold green]Completionist: [/bold green][white]{best.completionist}h[/white]"
+            )
+        # await asyncio.sleep(0.5)
+
+
+async def main():
     print("[bold blue]Hello from steam-cli![/bold blue]")
     console.print("[italics]Made by Kangy[/italics]")
 
@@ -37,31 +55,17 @@ def main():
         }
         for game in games_list
     ]
-    # for game in skimmed_library:
-    #     print("==============================")
-    #     console.print(f"Game: {game.get("name")}")
-    #     console.print(f"Playtime: {game.get("playtime") / 60:.2f} hours")
-    #     print("==============================")
 
-    # Once Steam API is implemented or planned, refactor the below so this file doesn't get too big!
     print("Searching for game...")
+    semaphore = asyncio.Semaphore(20)
     with Progress(console=console, transient=True) as progress:
         progress.add_task("", total=None)
-        for game in skimmed_library:
-            game_name = game.get("name")
-            playtime = game.get("playtime")
-            if not game_name:
-                continue
-            results = HowLongToBeat().search(game_name)
-            if not results:
-                console.print(f"[bold red]No game found:[/bold red] {game_name}")
-            else:
-                best = max(results, key=lambda e: e.similarity)
-                progress.console.print("[bold green]Game found![/bold green]")
-                progress.console.print(
-                    f"[bold blue]{best.game_name}[/bold blue] — Playtime: {playtime /60:.2f} hours -  [bold green]Main: [/bold green][white]{best.main_story}h,[/white] [bold green]Completionist: [/bold green][white]{best.completionist}h[/white]"
-                )
+        hltb_pulls = [
+            fetch_game_data(game, semaphore, progress) for game in skimmed_library
+        ]
+        await asyncio.gather(*hltb_pulls)
+    print("Sync complete!")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
